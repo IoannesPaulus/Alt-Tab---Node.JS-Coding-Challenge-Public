e40de1fc6c5d340e43c335998338563827e798db
Pál János <janos.pal@incepteam.com>
2018-02-01 02:11:53 +0100

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
     "extends": "airbnb-base",
+    "env": {
+        "mocha": true
+    },
     "rules": {
         "comma-dangle": 0,
         "prefer-const": 0,
         "prefer-destructuring": 0,
         "prefer-template": 0,
-        "no-underscore-dangle": 0
+        "no-underscore-dangle": 0,
+        "padded-blocks": 0
     }
 };
